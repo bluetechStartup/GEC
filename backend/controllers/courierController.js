5862e178bae2c2c985f869b0b629e84e8b891e3f
sopranoelfuego <aubinjaja@gmail.com>
2021-06-29 13:56:13 +0200

    backend(add annexe uploading file)

--- backend/controllers/courierController.js
+++ backend/controllers/courierController.js
@@ -1,6 +1,183 @@
+const multer = require('multer')
+const asyncHandler = require('express-async-handler')
+const Courrier = require('../models/courierModel.js')
 
+let getCouriers = asyncHandler(async (req, res, next) => {
+ Courrier.getAll((err, courriers) => {
+  response.status(200).send({
+   data: courriers,
+   message: `Les courriers`,
+   type: 'success',
+   success: true,
+  })
+ })
+})
 
+const createCourier = asyncHandler(async (req, res, next) => {
+ const {
+  MOUVEMENT_ID,
+  REFERENCE,
+  DATE_RECEPTION,
+  DATE_ENREGISTREMENT,
+  OBJET,
+  CATEGORIE_COURRIER_ID,
+  PRIORITE_ID,
+  CIVILITE_ID,
+  EXPEDITEUR_IDENTITE,
+  EXPEDITEUR_ADDRESSE,
+  EXPEDITEUR_VILLE_ID,
+  SERVICE_ID,
+  REFERENT_USER_ID,
+  ACTION_ID,
+  USER_ID,
+ } = req.body
 
+ var newCourrier = {
+  MOUVEMENT_ID: MOUVEMENT_ID,
+  REFERENCE: REFERENCE,
+  DATE_RECEPTION: DATE_RECEPTION,
+  DATE_COURRIER: new Date(),
+  DATE_ENREGISTREMENT: DATE_ENREGISTREMENT,
+  OBJET: OBJET,
+  CATEGORIE_COURRIER_ID: CATEGORIE_COURRIER_ID,
+  PRIORITE_ID: PRIORITE_ID,
+  CIVILITE_ID: CIVILITE_ID,
+  EXPEDITEUR_IDENTITE: EXPEDITEUR_IDENTITE,
+  EXPEDITEUR_ADDRESSE: EXPEDITEUR_ADDRESSE,
+  EXPEDITEUR_VILLE_ID: EXPEDITEUR_VILLE_ID,
+  SERVICE_ID: SERVICE_ID,
+  REFERENT_USER_ID: REFERENT_USER_ID,
+  ACTION_ID: ACTION_ID,
+  USER_ID: USER_ID,
+  STATUT_ID: 1,
+ }
 
+ Courrier.create(newCourrier, (err, data) => {
+  if (err) return next(new Error(err.message))
+  return res.json(data)
+ })
+})
 
+let update = asyncHandler(async (req, res, next) => {
+ const {
+  MOUVEMENT_ID,
+  REFERENCE,
+  DATE_RECEPTION,
+  DATE_ENREGISTREMENT,
+  OBJET,
+  CATEGORIE_COURRIER_ID,
+  PRIORITE_ID,
+  CIVILITE_ID,
+  EXPEDITEUR_IDENTITE,
+  EXPEDITEUR_ADDRESSE,
+  EXPEDITEUR_VILLE_ID,
+  SERVICE_ID,
+  REFERENT_USER_ID,
+  ACTION_ID,
+  USER_ID,
+ } = req.body
+ var newCourrier = {
+  MOUVEMENT_ID: MOUVEMENT_ID,
+  REFERENCE: REFERENCE,
+  DATE_RECEPTION: DATE_RECEPTION,
+  DATE_COURRIER: new Date(),
+  DATE_ENREGISTREMENT: DATE_ENREGISTREMENT,
+  OBJET: OBJET,
+  CATEGORIE_COURRIER_ID: CATEGORIE_COURRIER_ID,
+  PRIORITE_ID: PRIORITE_ID,
+  CIVILITE_ID: CIVILITE_ID,
+  EXPEDITEUR_IDENTITE: EXPEDITEUR_IDENTITE,
+  EXPEDITEUR_ADDRESSE: EXPEDITEUR_ADDRESSE,
+  EXPEDITEUR_VILLE_ID: EXPEDITEUR_VILLE_ID,
+  SERVICE_ID: SERVICE_ID,
+  REFERENT_USER_ID: REFERENT_USER_ID,
+  ACTION_ID: ACTION_ID,
+  USER_ID: USER_ID,
+  STATUT_ID: 1,
+ }
+ Courrier.update(newCourrier, req.params.id, (err, data) => {
+  if (err) return next(new Error(err.message))
+  return res.json(data)
+ })
+})
 
+// A REVOIR ET METTRE MULTER
+let createAnnexe = (req, res) => {
+ const { COURRIER_ID, NOM_PIECE, TYPE_ANNEXE_ID, CATEGORIE_ANNEXE_ID } =
+  req.body
+
+ console.log('file out here', req.file)
+
+ const annexeFormats = [
+  'audio/midi',
+  'audio / mpeg',
+  'audio / webm',
+  'audio / ogg',
+  'audio / wav',
+  'video/webm',
+  'video/x-msvideo',
+  'video/ogg',
+ ]
+ if (annexeFormats.includes(req.file.mimetype.trim())) {
+  return res
+   .status(404)
+   .json({ success: false, message: 'seule le document peuvent etre choisi!!' })
+ }
+ PATH = `${process.env.BASE_URL}/${req.file.path}`
+ console.log('the path:', PATH)
+}
+
+let getAnnexes = (req, res) => {
+ let id = req.params.id
+ Courrier.getAnnexes(id, (error, courrier) => {
+  if (error) return next(new Error(error.message))
+  res.status(200).send(courrier)
+ })
+}
+let getById = asyncHandler(async (req, res, next) => {
+ Courrier.getById(req.params.id, (err, data) => {
+  if (err) return cb(error, null)
+
+  res.status(200).json(data)
+ })
+})
+
+let removeAnnexe = (req, res) => {
+ let id = req.params.id
+ Courrier.removeAnnexe(id, (error, annexe) => {
+  res.status(200).send({
+   data: annexe,
+   message: `L'annexe du courrier a été supprimé.`,
+   type: 'success',
+   success: true,
+  })
+ })
+}
+let removeCourrier = asyncHandler(async (req, res, next) => {
+ Courrier.removeCourrier(req.params.id, (error, courrier) => {
+  const { data } = courrier
+  if (error) return next(new Error(error.message))
+
+  if (data.affectedRows > 0) {
+   Courrier.removeAnnexes(id, (error, annexes) => {
+    if (error) return next(new Error(error.message))
+   })
+   return res.json(courrier)
+  } else {
+   res
+    .status(200)
+    .json({ success: false, message: "le courrier n'est pas suprimer" })
+  }
+ })
+})
+
+module.exports = {
+ getById,
+ createAnnexe,
+ getAnnexes,
+ removeCourrier,
+ removeAnnexe,
+ getCouriers,
+ createCourier,
+ update,
+}
--- backend/models/courierModel.js
+++ backend/models/courierModel.js
@@ -1,112 +1,110 @@
-let connection = require("../config/db");
+let connection = require('../config/db')
 
-class Mdl_courrier {
-  static getAll(cb) {
-    connection.query(
-      `SELECT cr.* , ct.COURRIER_DESCR,sv.SERVICE_DESCR, cv.CIVILITE_DESCR, st.STATUT_DESCR, pr.PRIORITE_DESCR FROM cr_courriers AS cr 
+class Courrier {
+ static getAll(cb) {
+  connection.query(
+   `SELECT cr.* , ct.COURRIER_DESCR,sv.SERVICE_DESCR, cv.CIVILITE_DESCR, st.STATUT_DESCR, pr.PRIORITE_DESCR FROM cr_courriers AS cr 
       JOIN cr_categorie_courier AS ct ON ct.CATEGORIE_COURRIER_ID = cr.CATEGORIE_COURRIER_ID 
       JOIN services AS sv ON sv.SERVICE_ID = cr.SERVICE_ID 
       JOIN cr_civilite AS cv ON cv.CIVILITE_ID = cr.CIVILITE_ID  
       JOIN cr_statut AS st ON st.STATUT_ID = cr.STATUT_ID 
       JOIN cr_priorite AS pr ON pr.PRIORITE_ID = cr.PRIORITE_ID 
       ORDER BY cr.COURRIER_ID DESC`,
-      (error, methodes) => {
-        if (error) throw error;
-        cb(error, methodes);
-      }
-    );
-  }
+   (error, methodes) => {
+    if (error) throw error
+    cb(error, methodes)
+   }
+  )
+ }
 
-//   PROFIL_ID,FONCTIONNALITE_ID ,STRING
-  static getOne(id, cb) {
-    connection.query(
-      `SELECT * FROM cr_courriers WHERE COURRIER_ID =?  ORDER BY COURRIER_ID DESC`,[id],
-      (error, methodes) => {
-        if (error) throw error;
-        cb(error, methodes);
-      }
-    );
-  }
+ //   PROFIL_ID,FONCTIONNALITE_ID ,STRING
+ static getById(id, cb) {
+  connection.query(
+   `SELECT * FROM cr_courriers WHERE COURRIER_ID =?  ORDER BY COURRIER_ID DESC`,
+   [id],
+   (error, methodes) => {
+    if (error) throw error
+    cb(error, methodes)
+   }
+  )
+ }
 
-  static edit(data, id, cb) {
-    connection.query(
-      `UPDATE cr_courriers SET ? WHERE COURRIER_ID =?`,
-      [data, id],
-      (error, courrier) => {
-        if (error) throw error;
-        cb(error, courrier);
-      }
-    );
-  }
+ static update(data, id, cb) {
+  connection.query(
+   `UPDATE cr_courriers SET ? WHERE COURRIER_ID =?`,
+   [data, id],
+   (error, courrier) => {
+    if (error) throw error
+    cb(error, courrier)
+   }
+  )
+ }
 
-  static insert(data, cb) {
-    connection.query(
-      "INSERT cr_courriers SET ?",
-      [data],
-      (error, fonctionnalite) => {
-        if (error) throw error;
-        cb(error, fonctionnalite);
-      }
-    );
-  }
+ static create(data, cb) {
+  connection.query('INSERT cr_courriers SET ?', [data], (err, data) => {
+   if (err) return cb(err, null)
+   cb(error, { success: true, data })
+  })
+ }
 
-  static insertAnnexe(data, cb) {
-    connection.query(
-      "INSERT cr_courrier_annexe SET ?",
-      [data],
-      (error, courrier_annexe) => {
-        if (error) throw error;
-        cb(error, courrier_annexe);
-      }
-    );
-  }
+ static createAnnexe(data, cb) {
+  connection.query(
+   'INSERT cr_courrier_annexe SET ?',
+   [data],
+   (error, courrier_annexe) => {
+    if (error) throw error
+    cb(error, courrier_annexe)
+   }
+  )
+ }
 
-  static getAnnexes(id, cb) {
-    connection.query(
-      `SELECT an.*,ct.CATEGORIE_ANNEXE_DESCR,ty.TYPE_ANNEXE_DESCR FROM cr_courrier_annexe AS an 
+ static getAnnexes(id, cb) {
+  connection.query(
+   `SELECT an.*,ct.CATEGORIE_ANNEXE_DESCR,ty.TYPE_ANNEXE_DESCR FROM cr_courrier_annexe AS an 
       JOIN cr_categorie_annexe AS ct ON ct.CATEGORIE_ANNEXE_ID = an.CATEGORIE_ANNEXE_ID 
       JOIN cr_type_annexe AS ty ON ty.TYPE_ANNEXE_ID = an.TYPE_ANNEXE_ID 
       WHERE an.COURRIER_ID = ? ORDER BY COURRIER_ANNEXE_ID DESC`,
-      [id],
-      (error, annexes) => {
-        if (error) throw error;
-        cb(error, annexes);
-      }
-    );
-  }
+   [id],
+   (error, data) => {
+    if (error) return cb(error, null)
+    cb(null, { success: true, data })
+   }
+  )
+ }
 
-  static removeCourrier(id, cb) {
-    connection.query(
-      "DELETE FROM cr_courriers WHERE COURRIER_ID = ?",
-      [id],
-      (error, courrier) => {
-        if (error) throw error;
-        cb(error, courrier);
-      }
-    );
-  }
+ static removeCourrier(id, cb) {
+  connection.query(
+   'DELETE FROM cr_courriers WHERE COURRIER_ID = ?',
+   [id],
+   (error, courrier) => {
+    if (error) return cb(error, null)
 
-  static removeAnnexes(id, cb) {
-    connection.query(
-      "DELETE FROM cr_courrier_annexe WHERE COURRIER_ID = ?",
-      [id],
-      (error, annexes) => {
-        if (error) throw error;
-        cb(error, annexes);
-      }
-    );
-  }
+    cb(error, courrier)
+   }
+  )
+ }
 
-  static removeAnnexe(id, cb) {
-    connection.query(
-      "DELETE FROM cr_courrier_annexe WHERE COURRIER_ANNEXE_ID = ?",
-      [id],
-      (error, annexe) => {
-        if (error) throw error;
-        cb(error, annexe);
-      }
-    );
-  }
+ static removeAnnexes(id, cb) {
+  connection.query(
+   'DELETE FROM cr_courrier_annexe WHERE COURRIER_ID = ?',
+   [id],
+   (error, data) => {
+    if (error) return cb(error, null)
+    cb(error, { success: true, data })
+   }
+  )
+ }
+
+ static removeAnnexe(id, cb) {
+  connection.query(
+   'DELETE FROM cr_courrier_annexe WHERE COURRIER_ANNEXE_ID = ?',
+   [id],
+   (error, annexe) => {
+    if (error) throw error
+    cb(error, annexe)
+   }
+  )
+ }
 }
 
-module.exports = Mdl_courrier;
+module.exports = Courrier
